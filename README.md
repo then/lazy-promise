4d75362e4a5590b74ebd2831d1c2a327b4c9f0ea
Nathan Zadoks <nathan@nathan7.eu>
2013-04-04 17:09:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# lazy-promise
+
+  A bare-bones lazily-evaluated Promises/A+ implementation.
+
+## Installation
+
+    $ npm install lazy-promise
+
+  or
+
+    $ component install nathan7/lazy-promise
+
+## API
+
+### new Promise(function(resolve, reject))
+
+  Create a new lazy promise with the given function.
+
+## License
+
+  MIT
+
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = LazyPromise
+function LazyPromise(fn) {
+  if (!(this instanceof LazyPromise))
+    return new LazyPromise(fn)
+  if (typeof fn !== 'function')
+    throw new TypeError('fn is not a function')
+
+  var promise = null
+  this.then = function(onResolved, onRejected) {
+    ;(promise = promise || new Promise(fn)).then(onResolved, onRejected)
+  }
+}
