423f196b6067b97f9e68eaf194db468847c6750c
Nathan Zadoks <nathan@nathan7.eu>
2013-04-04 20:27:28 +0200

    [fix] ensure factory is called asynchronously

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var Promise = require('promise')
+  , nextTick = (typeof process !== 'undefined' && typeof process.nextTick === 'function')
+      ? process.nextTick
+      : require('next-tick')
+
 module.exports = LazyPromise
 function LazyPromise(fn) {
   if (!(this instanceof LazyPromise))
@@ -8,6 +12,15 @@ function LazyPromise(fn) {
 
   var promise = null
   this.then = function(onResolved, onRejected) {
-    return (promise = promise || new Promise(fn)).then(onResolved, onRejected)
+    if (promise === null) createPromise()
+    return promise.then(onResolved, onRejected)
+  }
+
+  function createPromise() {
+    promise = new Promise(function(resolve, reject) {
+      nextTick(function() {
+        fn(resolve, reject)
+      })
+    })
   }
 }
